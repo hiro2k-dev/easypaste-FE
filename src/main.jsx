8405c38371251a5100c7b351a45804e2c156e9ce
hiro <huyp26102000@gmail.com>
2025-08-16 22:26:00 +0700

    update GA

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import SessionPage from "./SessionPage.jsx";
 import HomePage from "./HomePage.jsx";
+import ReactGA from "react-ga4";
 
+const GA_CODE = import.meta.env.VITE_GA_CODE;
+console.log(GA_CODE)
+ReactGA.initialize(`G-${GA_CODE}`);
+ReactGA.send("pageview");
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
